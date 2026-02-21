abe32c33d441029e90ff0f2a94b5f94e37c82f50
ansufw <ansufw@gmail.com>
2026-02-21 08:33:39 +0100

    first commit

---
+++ modules/WebClient/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Costumer customer = new Costumer();
+        customer.setName("Ananto YW");
+        System.out.println(customer.getName());
+    }
+}
